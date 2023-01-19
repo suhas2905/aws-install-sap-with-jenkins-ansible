ba7b93a9c741ecd75e766fd316381f0e162a56ab
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-19 01:39:46 -0500

    Update check_SSH_KEYPAIR_FILE.sh

--- jenkins-pipelines/sap-pipeline-hana-pas-ascs/stages/prepare/check_global_variables/check_SSH_KEYPAIR_FILE.sh
+++ jenkins-pipelines/sap-pipeline-hana-pas-ascs/stages/prepare/check_global_variables/check_SSH_KEYPAIR_FILE.sh
@@ -13,3 +13,11 @@ fi
 
 echo "$SSH_KEYPAIR_FILE"
 exit 0
+
+if [ -z "$SSH_KEYPAIR_FILE_DR" ]; then
+    echo "Invalid file for KeyPair for DR. You must upload a file already existing on https://console.aws.amazon.com/ec2/v2/home#KeyPairs:"
+    exit 100
+fi
+
+echo "$SSH_KEYPAIR_FILE_DR"
+exit 0
