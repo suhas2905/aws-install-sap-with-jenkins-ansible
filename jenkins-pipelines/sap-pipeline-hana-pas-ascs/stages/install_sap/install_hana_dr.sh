60b5a821ca8a5df5839f5cefe40c0459422ea18f
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-24 13:21:35 -0500

    Update install_hana_dr.sh

--- jenkins-pipelines/sap-pipeline-hana-pas-ascs/stages/install_sap/install_hana_dr.sh
+++ jenkins-pipelines/sap-pipeline-hana-pas-ascs/stages/install_sap/install_hana_dr.sh
@@ -3,7 +3,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-ansibleHanaDirdr="$PWD/ansible-playbooks/aws-sap-hana-da"
+ansibleHanaDirdr="$PWD/ansible-playbooks/aws-sap-dr-hana"
 export ANSIBLE_DIR=$ansibleHanaDirdr
 
 # ------------------------------------------------------------------
